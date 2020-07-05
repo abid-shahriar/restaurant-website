afccf75829accaf12ca774e6335ff97b0222ba15
abid7 <abidshahriar7@gmail.com>
2020-07-05 21:41:20 +0600

    modified food menu section

--- js/foodMenu.js
+++ js/foodMenu.js
@@ -3,27 +3,26 @@ import { foodData } from "./data.js";
 window.addEventListener("DOMContentLoaded", () => {
   // food menu section
   {
-    const menuAppeteasers = document.querySelector(".menu-appeteasers");
-    const menuMainDish = document.querySelector(".menu-mainDish");
-    const menuDessert = document.querySelector(".menu-dessert");
-    const menuDrinks = document.querySelector(".menu-drinks");
+    const menuAppeteasers = document.querySelector(".menu-appeteasers .dishes");
+    const menuMainDish = document.querySelector(".menu-mainDish .dishes");
+    const menuDessert = document.querySelector(".menu-dessert .dishes");
+    const menuDrinks = document.querySelector(".menu-drinks .dishes");
 
-    foodData.slice(0, 9).forEach((food) => {
+    foodData.slice(0, 12).forEach((food) => {
       insertTo(menuAppeteasers, food);
     });
-    foodData.slice(7, 16).forEach((food) => {
+    foodData.slice(7, 19).forEach((food) => {
       insertTo(menuMainDish, food);
     });
-    foodData.slice(6, 15).forEach((food) => {
+    foodData.slice(6, 18).forEach((food) => {
       insertTo(menuDessert, food);
     });
-    foodData.slice(10, 19).forEach((food) => {
+    foodData.slice(3, 15).forEach((food) => {
       insertTo(menuDrinks, food);
     });
 
     function insertTo(item, food) {
-      let insertItem = `<div class="dishes">
-    <div class="item">
+      let insertItem = `<div class="item">
       <div class="info">
         <h2>${food.title}</h2>
         <p>
@@ -31,10 +30,9 @@ window.addEventListener("DOMContentLoaded", () => {
         </p>
       </div>
       <div class="price">
-        <p>$${food.price}</p>
+        <p>${food.price}$</p>
       </div>
-    </div>
-  </div>`;
+    </div>`;
 
       item.insertAdjacentHTML("beforeend", insertItem);
     }
